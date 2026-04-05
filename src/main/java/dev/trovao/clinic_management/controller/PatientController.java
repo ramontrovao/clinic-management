9a7a6cb4cdd817a75d9aed59ad8dca7d511da1dd
ramon trovão <ramonzinhg12@gmail.com>
2026-04-04 23:00:22 -0300

    docs: add Swagger documentation to getAllPatients endpoint

--- src/main/java/dev/trovao/clinic_management/controller/PatientController.java
+++ src/main/java/dev/trovao/clinic_management/controller/PatientController.java
@@ -2,8 +2,15 @@ package dev.trovao.clinic_management.controller;
 
 import dev.trovao.clinic_management.domain.patient.dto.PatientDTO;
 import dev.trovao.clinic_management.domain.patient.dto.PatientRequestDTO;
+import dev.trovao.clinic_management.exception.model.response.ArgumentNotValidErrorResponse;
 import dev.trovao.clinic_management.exception.patient.PatientNotFoundException;
 import dev.trovao.clinic_management.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,16 +22,27 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Patient Management", description = "APIs for managing patients")
 @RequestMapping("/patients")
 public class PatientController {
     private final PatientService patientService;
 
     @PostMapping
+    @Operation(summary = "Create a new patient" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patient created successfully",
+                    content = @Content(schema = @Schema(implementation = PatientDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request data",
+                    content = @Content(schema = @Schema(implementation = ArgumentNotValidErrorResponse.class)))
+    })
     public ResponseEntity<PatientDTO> createPatient(@RequestBody @Valid PatientRequestDTO patientDto) {
         return new ResponseEntity<>(patientService.createPatient(patientDto), HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Get all patients")
+    @ApiResponse(responseCode = "200", description = "List of all patients",
+            content = @Content(schema = @Schema(implementation = PatientDTO.class)))
     public ResponseEntity<List<PatientDTO>> getAllPatients() {
         return ResponseEntity.ok(patientService.getAllPatients());
     }
