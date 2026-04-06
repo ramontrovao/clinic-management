a3200ae1d44fd520178cd654883e79c37cb3a1c6
ramon trovão <ramonzinhg12@gmail.com>
2026-04-05 22:53:04 -0300

    feat: add DoctorRepository with JpaRepository

---
+++ src/main/java/dev/trovao/clinic_management/repository/DoctorRepository.java
@@ -0,0 +1,8 @@
+package dev.trovao.clinic_management.repository;
+
+import dev.trovao.clinic_management.domain.doctor.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DoctorRepository extends JpaRepository<Doctor, UUID> {}
